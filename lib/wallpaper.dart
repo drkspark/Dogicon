211f4f335f3640a9a30961f0e20e84bc4a245f8e
drkspark <madhusudhanrock753@gmail.com>
2022-04-11 10:49:40 +0530

    Added API and Fetched Images to GridView

---
+++ lib/wallpaper.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Wallpaper extends StatefulWidget {
+  const Wallpaper({Key? key}) : super(key: key);
+
+  @override
+  _WallpaperState createState() => _WallpaperState();
+}
+
+class _WallpaperState extends State<Wallpaper> {
+  List images = [];
+  int page = 1;
+  @override
+  void initState() {
+    super.initState();
+    fetchapi();
+  }
+
+  fetchapi() async {
+    await http.get(Uri.parse('https://api.pexels.com/v1/curated?per_page=80'),
+        headers: {
+          'Authorization':
+              '563492ad6f91700001000001cc636c789a9242fa8419a73531c5f266'
+        }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images = result['photos'];
+      });
+      print(images[0]);
+    });
+  }
+
+  loadmore() async {
+    setState(() {
+      page = page + 1;
+    });
+    String url =
+        'https://api.pexels.com/v1/curated?per_page=80&page=' + page.toString();
+    await http.get(Uri.parse(url), headers: {
+      'Authorization':
+          '563492ad6f91700001000001cc636c789a9242fa8419a73531c5f266'
+    }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images.addAll(result['photos']);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+                itemCount: images.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 2,
+                    crossAxisCount: 3,
+                    childAspectRatio: 2 / 3,
+                    mainAxisSpacing: 2),
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: Colors.white,
+                    child: Image.network(
+                      images[index]['src']['tiny'],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }),
+          ),
+          InkWell(
+            onTap: () {
+              loadmore();
+            },
+            child: Container(
+              alignment: Alignment.center,
+              height: 60,
+              width: double.infinity,
+              color: Colors.black,
+              child: const Center(
+                child: Text(
+                  'Load More',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
